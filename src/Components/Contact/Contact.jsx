af707e8b3d564eba565c30507f3323e2efc1bbf0
jinto peter <jinto.peter3@gmail.com>
2024-06-13 07:49:59 +0530

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+const Contact = () => {
+
+        const [result, setResult] = React.useState("");
+      
+        const onSubmit = async (event) => {
+          event.preventDefault();
+          setResult("Sending....");
+          const formData = new FormData(event.target);
+      
+          formData.append("access_key", "25c7ffc4-6dab-4b73-af74-e3fde68ef733");
+      
+          const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+          });
+      
+          const data = await response.json();
+      
+          if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+          } else {
+            console.log("Error", data);
+            setResult(data.message);
+          }
+        };
+      
+
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+        <p>Feel free to reach out through contact form or find our
+            contact information below. Your feedback, question, and 
+            suggestions are important to us as we strive to provide 
+            exceptional service to our  university community.
+        </p>
+        <ul>
+            <li> <img src={mail_icon} alt="" />jinto.peter3@gmail.com</li>
+            <li><img src={phone_icon} alt="" />+91 9497347739</li>
+            <li><img src={location_icon} alt="" />Kuttikkatt P. O Arimbur <br />
+            680620 Thrissur,Kerala,India</li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+            <label>Your Name</label>
+            <input type="text" name='name' placeholder=
+            'Enter your name' 
+            required />
+            <label>Phone Number</label>
+            <input type="text" name='phone' placeholder=
+            'Enter your number' 
+            required />
+            <label>Write your mesasage</label>
+            <textarea name="message"  rows="6" placeholder=
+            'Enter your message' required></textarea>
+            <button type='submit' className='btn dark-btn'>
+                Submit now</button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>Our cutting-edge cutticulm is designed to empower students with the knoledge, skills, and experiences needed to excel in the dynamic filed of edication</p>
+          <button className='btn'>Explore more.. <img src={dark_arrow} alt="" /></button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
